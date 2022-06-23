0c22f7fa925659b6d79fb21b82e637e34606386e
PinoLabs <pino440@gmail.com>
2022-06-23 18:41:30 -0400

    ate

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function MostrarSM(){
+    document.getElementById('sobre-mi').style.display = 'block';
+    document.getElementById('educacion').style.display = 'none';
+    document.getElementById('proyectos').style.display = 'none';
+    document.getElementById('experiencia').style.display = 'none';
+    document.getElementById('contacto').style.display = 'none';
+    } 
+function MostrarE(){
+    document.getElementById('sobre-mi').style.display = 'none';
+    document.getElementById('educacion').style.display = 'block';
+    document.getElementById('proyectos').style.display = 'none';
+    document.getElementById('experiencia').style.display = 'none';
+    document.getElementById('contacto').style.display = 'none';
+}
+function MostrarP(){
+    document.getElementById('sobre-mi').style.display = 'none';
+    document.getElementById('educacion').style.display = 'none';
+    document.getElementById('proyectos').style.display = 'block';
+    document.getElementById('experiencia').style.display = 'none';
+    document.getElementById('contacto').style.display = 'none';
+}
+function MostrarEx(){
+    document.getElementById('sobre-mi').style.display = 'none';
+    document.getElementById('educacion').style.display = 'none';
+    document.getElementById('proyectos').style.display = 'none';
+    document.getElementById('experiencia').style.display = 'block';
+    document.getElementById('contacto').style.display = 'none';
+}
+function MostrarC(){
+    document.getElementById('sobre-mi').style.display = 'none';
+    document.getElementById('educacion').style.display = 'none';
+    document.getElementById('proyectos').style.display = 'none';
+    document.getElementById('experiencia').style.display = 'none';
+    document.getElementById('contacto').style.display = 'block';
+}
